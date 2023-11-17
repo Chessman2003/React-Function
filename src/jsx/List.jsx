600f2f34519695be43ca7575ee4c5e2e7ba1650c
Ivancov Alexander <ivancovae@kodeks.ru>
2023-11-17 17:45:06 +0300

    refactoring or fix bugs

--- src/jsx/List.jsx
+++ src/jsx/List.jsx
@@ -1,31 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ListItem from './ListItem';
 
 
 const List = () => {
-    const {items, setItems} = useState([]);
+    const [items, setItems] = useState([]);
 
     const handleAddItem = () => {
         const newItem = {
-            id: items.index + 1,
+            id: items.length,
             title: `Заголовок ${items.length + 1}`,
             text: `Текст ${items.length + 1}`
         };
-        setItems(...items, newItem);
+        setItems([...items, newItem]);
     };
-        
+
     const handleDeleteItem = (id) => {
-            const updatedItems = items.filter(item => item.id !== id);
-            setItems(updatedItems);
+        const updatedItems = items.filter(item => item.id !== id);
+        setItems(updatedItems);
     };
-    return(
+    return (
         <div>
             <button onClick={handleAddItem}>Добавить элемент</button>
-            {items.map(item => (
-        <ListItem key={item.id} item={item} onDelete={handleDeleteItem} />
-    ))}
+            {
+                items.map(item => (
+                    <ListItem
+                        key={item.id}
+                        item={item}
+                        onDelete={handleDeleteItem}
+                    />
+                ))
+            }
         </div>
-  );
+    );
 };
 
 
